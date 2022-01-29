ec61cc3555f8dc9730b9f88e01bfd6eaff656d11
David F <account@davidfdev.com>
2022-01-29 11:36:48 +0000

    Update Interactable.cs

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    #region Static methods
+
+    /// <summary>
+    ///     Search for an interactable at the given position, returning the closest one if there are multiple.
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="radius"></param>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    public static Interactable At(Vector3 pos, float radius, InteractableState state)
+    {
+        return Physics.OverlapSphere(pos, radius)
+            .Where(x => x.GetComponent<Interactable>() != null && (x.GetComponent<Interactable>().AllowedInteractions & state) != 0)
+            .OrderBy(x => Vector2.Distance(pos, x.transform.position))
+            .FirstOrDefault()
+            ?.GetComponent<Interactable>();
+    }
+
+    #endregion
+
     #region Properties
 
     /// <summary>
@@ -28,13 +49,29 @@ public abstract class Interactable : MonoBehaviour
         private set => State = value ? (State | InteractableState.Keyboard) : (State & ~InteractableState.Keyboard);
     }
 
+    /// <summary>
+    ///     Whether mouse interactions are allowed.
+    /// </summary>
+    public bool IsMouseAllowed => (AllowedInteractions & InteractableState.Mouse) != 0;
+
+    /// <summary>
+    ///     Whether keyboard interactions are allowed.
+    /// </summary>
+    public bool IsKeyboardAllowed => (AllowedInteractions & InteractableState.Keyboard) != 0;
+
+    /// <summary>
+    ///     Allowed interactions.
+    /// </summary>
+    [field: SerializeField]
+    public InteractableState AllowedInteractions { get; private set; } = InteractableState.Both;
+
     #endregion
 
     #region Methods
 
     public void StartMouseInteraction()
     {
-        if (IsMouseInteracting)
+        if (IsMouseInteracting || !IsMouseAllowed)
         {
             return;
         }
@@ -58,7 +95,7 @@ public abstract class Interactable : MonoBehaviour
 
     public void StartKeyboardInteraction()
     {
-        if (IsKeyboardInteracting)
+        if (IsKeyboardInteracting || !IsKeyboardAllowed)
         {
             return;
         }
