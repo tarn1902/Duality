5b86a0f09d32bc3197507aa72763baebaacd0b6e
David F <account@davidfdev.com>
2022-01-29 11:16:22 +0000

    Update RopeInteractable.cs

--- Assets/Scripts/Interactions/RopeInteractable.cs
+++ Assets/Scripts/Interactions/RopeInteractable.cs
@@ -7,22 +7,18 @@ public sealed class RopeInteractable : Interactable
 
     protected override void OnStartMouseInteraction(MousePlayer mousePlayer, bool onlyMouse)
     {
-        throw new System.NotImplementedException();
     }
 
     protected override void OnEndMouseInteraction(MousePlayer mousePlayer, bool onlyMouse)
     {
-        throw new System.NotImplementedException();
     }
 
     protected override void OnStartKeyboardInteraction(KeyboardPlayer keyboardPlayer, bool onlyKeyboard)
     {
-        throw new System.NotImplementedException();
     }
 
     protected override void OnEndKeyboardInteraction(KeyboardPlayer keyboardPlayer, bool onlyKeyboard)
     {
-        throw new System.NotImplementedException();
     }
 
     #endregion
