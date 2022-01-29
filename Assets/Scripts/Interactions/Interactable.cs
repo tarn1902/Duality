d95dac5d777c6a2522d05f2bcb4c7cb0a248b803
David F <account@davidfdev.com>
2022-01-29 10:48:44 +0000

    Update Interactable.cs

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -3,13 +3,62 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    #region Fields
+    #region Properties
+
+    /// <summary>
+    ///     Current interaction state.
+    /// </summary>
+    public InteractableState State { get; private set; } = InteractableState.None;
+
+    /// <summary>
+    ///     Whether the mouse is currently interacting with the interactable.
+    /// </summary>
+    public bool IsMouseInteracting
+    {
+        get => (State & InteractableState.Mouse) != 0;
+        private set => State = value ? (State | InteractableState.Mouse) : (State & ~InteractableState.Mouse);
+    }
+
+    /// <summary>
+    ///     Whether the keyboard is currently interacting with the interactable.
+    /// </summary>
+    public bool IsKeyboardInteracting
+    {
+        get => (State & InteractableState.Keyboard) != 0;
+        private set => State = value ? (State | InteractableState.Keyboard) : (State & ~InteractableState.Keyboard);
+    }
 
     #endregion
 
     #region Methods
 
+    public void StartMouseInteraction()
+    {
 
+    }
+
+    public void EndMouseInteraction()
+    {
+        
+    }
+
+    public void StartKeyboardInteraction()
+    {
+
+    }
+
+    public void EndKeyboardInteraction()
+    {
+        
+    }
+
+    protected abstract void OnStartMouseInteraction();
+
+    protected abstract void OnEndMouseInteraction();
+
+    protected abstract void OnStartKeyboardInteraction();
+
+    protected abstract void OnEndKeyboardInteraction();
 
     #endregion
 
