90d716c8e225f9790e6934d7d509dabd644b21d2
David F <account@davidfdev.com>
2022-01-30 16:43:08 +1100

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
+    #region Fields
+
+    private float _minY;
+
+    #endregion
+
     #region Properties
 
     [field: SerializeField]
@@ -15,6 +21,11 @@ public class CameraFollow : MonoBehaviour
 
     #region Methods
 
+    private void Awake()
+    {
+        _minY = transform.position.y;
+    }
+
     private void LateUpdate()
     {
         if (Target == null)
@@ -23,7 +34,15 @@ public class CameraFollow : MonoBehaviour
         }
 
         Vector3 pos = transform.position;
+        
         pos.x = Mathf.Lerp(pos.x, Target.position.x, Time.deltaTime * FollowSpeed);
+        pos.y = Mathf.Lerp(pos.y, Target.position.y, Time.deltaTime * FollowSpeed);
+
+        if (pos.y < _minY)
+        {
+            pos.y = _minY;
+        }
+
         transform.position = pos;
     }
 
