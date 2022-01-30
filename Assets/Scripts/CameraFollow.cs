487aa1ca1d43c7636299db3182980c0da41461ad
David F <account@davidfdev.com>
2022-01-30 16:40:02 +1100

    Camera Follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+    #region Properties
+
+    [field: SerializeField]
+    public Transform Target { get; set; }
+
+    [field: SerializeField]
+    public float FollowSpeed { get; set; } = 10f;
+
+    #endregion
+
+    #region Methods
+
+    private void LateUpdate()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Lerp(pos.x, Target.position.x, Time.deltaTime * FollowSpeed);
+        transform.position = pos;
+    }
+
+    #endregion
+}
